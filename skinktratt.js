d9d70ad4113af8fd4515af8a922aa6a1e6d0a62f
André Karlsson <andre.karlsson@brandairport.se>
2013-08-26 22:35:38 +0200

    settings WIP

---
+++ skinktratt.js
@@ -0,0 +1,35 @@
+if(Meteor.isServer){
+	var App = new Meteor.Collection("app");
+
+	Meteor.publish('app', function(){
+		return App.find({})
+	})
+
+
+	
+}
+
+if(Meteor.isClient){
+
+	var pages = ['#home', '#aliases', '#stats', '#settings'];
+
+	var App = Meteor.subscribe('app', function(){
+		console.log('done');
+	});
+
+	Template['nav'].events({
+		"click .pages li": function (e, tmpl) {
+			$('.pages').find('.active').removeClass('active')
+			$(e.target).parent().toggleClass('active');
+			
+			var id = $(e.target).prop('hash');
+
+			pages.forEach(function (page) {
+				$(page).css('display', 'none');
+			});
+			$(id).css('display', 'block');
+
+		}
+	});
+
+}
