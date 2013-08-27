7cdf675d2f21daf8ddd5bd9073582d8b99e46d61
Andre Karlsson <andre.karlsson@brandairport.se>
2013-08-27 15:30:39 +0200

    Better login and registration

--- skinktratt.js
+++ skinktratt.js
@@ -3,10 +3,17 @@ if(Meteor.isServer){
 
 	Meteor.publish('app', function(){
 		return App.find({})
-	})
+	});
 
-
-	
+	Meteor.methods({
+		registered: function(username){
+			console.log(username)
+			if(Meteor.users.find({username:username}).count() > 0)
+				return true
+			else
+				return false
+		}
+	});
 }
 
 if(Meteor.isClient){
@@ -29,7 +36,57 @@ if(Meteor.isClient){
 			});
 			$(id).css('display', 'block');
 
+		},
+		'click .actions li': function(e){
+			Meteor.logout();
 		}
 	});
+
+	Template['login'].events = {
+		'blur #email': function(e){
+			Meteor.call('registered', $('#email').val(), function(err, res){
+				console.log(res)
+				Session.set('registered', res);
+			})
+		},		
+		'blur #password': function(e){
+			Meteor.call('registered', $('#email').val(), function(err, res){
+				console.log(res)
+				Session.set('error', null);
+			})
+		},
+		'submit': function(e){
+			e.preventDefault();
+			if(e.target.id == 'frm-login'){
+				console.log('login')
+				Meteor.loginWithPassword($('#email').val(), $('#password').val(), function(err){
+					if(err){
+						Session.set('error', 'loginError');
+					}
+				})
+			}else{
+				Accounts.createUser({
+					password: $('#password').val(),
+					email: $('#email').val(),
+					username: $('#email').val(),
+				}, function (err) {
+					if(err){
+						console.log('error creating user')
+					}
+				});
+			}
+			return false
+		}
+
+	}
+
+	Template['login'].helpers({
+		'registered': function(){
+			return Session.get('registered');
+		},
+		'error': function(){
+			return Session.get('error');
+		}
+	});	
 
 }
